217a127f3643f1d8fdf18a3581b070d48664304e
Mayaqq <i@mayaqq.dev>
2025-01-27 18:06:47 +0100

    why did it not push this

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,12 +16,6 @@ cloche {
         modId.set("cynosure")
     }
 
-    val fabricCommon = common("fabric:common") {
-        dependencies {
-            fabricApi("0.92.2+1.20.1")
-        }
-    }
-
     fabric("fabric:1.20.1") {
         loaderVersion.set("0.16.9")
         minecraftVersion.set("1.20.1")
@@ -32,35 +26,11 @@ cloche {
         dependencies {
             fabricApi("0.92.2+1.20.1")
         }
-
-        dependsOn(fabricCommon)
-    }
-
-    fabric("fabric:1.21.1") {
-        loaderVersion.set("0.16.9")
-        minecraftVersion.set("1.21.1")
-
-        client()
-        server()
-
-        dependencies {
-            fabricApi("0.110.0+1.21.1")
-        }
-
-        dependsOn(fabricCommon)
     }
 
     forge("forge:1.20.1") {
         loaderVersion.set("47.1.3")
         minecraftVersion.set("1.20.1")
-
-        client()
-        server()
-    }
-
-    neoforge("neoforge:1.21.1") {
-        loaderVersion.set("21.1.90")
-        minecraftVersion.set("1.21.1")
 
         client()
         server()
