29c9a284ff4cc8d3f36cfcedab3bbfa74e1ca6b0
sery <sery@sappho.company>
2025-03-06 03:02:56 +0100

    vertexconsumer.square for maya

--- src/common/main/kotlin/dev/mayaqq/cynosure/client/utils/VectorExtensions.kt
+++ src/common/main/kotlin/dev/mayaqq/cynosure/client/utils/VectorExtensions.kt
@@ -11,21 +11,21 @@ public fun Vector3f.grow(nx: Float, ny: Float, nz: Float): Vector3f =
 public fun Vector3f.shrink(nx: Float, ny: Float, nz: Float): Vector3f =
     set(nx.coerceAtMost(this.x), ny.coerceAtMost(this.y), nz.coerceAtMost(this.z))
 
-public inline operator fun Vector4fc.component0(): Float = x()
+public inline operator fun Vector4fc.component1(): Float = x()
 
-public inline operator fun Vector4fc.component1(): Float = y()
+public inline operator fun Vector4fc.component2(): Float = y()
 
-public inline operator fun Vector4fc.component2(): Float = z()
+public inline operator fun Vector4fc.component3(): Float = z()
 
-public inline operator fun Vector4fc.component3(): Float = w()
+public inline operator fun Vector4fc.component4(): Float = w()
 
-public inline operator fun Vector3fc.component0(): Float = x()
+public inline operator fun Vector3fc.component1(): Float = x()
 
-public inline operator fun Vector3fc.component1(): Float = y()
+public inline operator fun Vector3fc.component2(): Float = y()
 
-public inline operator fun Vector3fc.component2(): Float = z()
-
-public inline operator fun Vector2fc.component0(): Float = x()
+public inline operator fun Vector3fc.component3(): Float = z()
 
 public inline operator fun Vector2fc.component1(): Float = x()
+
+public inline operator fun Vector2fc.component2(): Float = x()
 
