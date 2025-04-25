41beb20bf1589b8913b9c7965ff8a825e0f71919
sery <sery@sappho.company>
2025-04-25 14:52:49 +0200

    bwah

--- common/src/main/kotlin/dev/mayaqq/cynosure/client/events/entity/RenderLayerRegistrationEvent.kt
+++ common/src/main/kotlin/dev/mayaqq/cynosure/client/events/entity/RenderLayerRegistrationEvent.kt
@@ -9,6 +9,7 @@ import net.minecraft.client.model.geom.EntityModelSet
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher
 import net.minecraft.client.renderer.entity.EntityRenderer
 import net.minecraft.client.renderer.entity.LivingEntityRenderer
+import net.minecraft.client.renderer.entity.RenderLayerParent
 import net.minecraft.client.renderer.entity.layers.RenderLayer
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.LivingEntity
@@ -19,19 +20,19 @@ import org.jetbrains.annotations.ApiStatus.NonExtendable
 public class RenderLayerRegistrationEvent(
     public val renderers: EntityRenderDispatcher,
     public val models: EntityModelSet,
-    private val context: Context
+    @PublishedApi internal val context: Context
 ) : Event {
 
-    public fun <T : LivingEntity> addLayer(entity: EntityType<T>, layer: RenderLayer<T, out EntityModel<T>>) {
-        (context.getEntity(entity) as? LivingEntityRendererAccessor)?.addLayer(layer)
+    public inline fun <T : LivingEntity> addLayer(entity: EntityType<T>, layer: (RenderLayerParent<T, EntityModel<T>>) -> RenderLayer<T, out EntityModel<T>>) {
+        context.getEntity(entity)?.let { (it as LivingEntityRendererAccessor).addLayer(layer(it)) }
     }
 
-    public fun addLayer(skin: DefaultSkin, layer: RenderLayer<out Player, out EntityModel<out Player>>) {
+    public inline fun addLayer(skin: DefaultSkin, layer: (RenderLayerParent<Player, EntityModel<Player>>) -> RenderLayer<out Player, out EntityModel<out Player>>) {
         addLayer(skin.skinName, layer)
     }
 
-    public fun addLayer(skin: String, layer: RenderLayer<out Player, out EntityModel<out Player>>) {
-        (context.getSkin(skin) as? LivingEntityRendererAccessor)?.addLayer(layer)
+    public inline fun addLayer(skin: String, layer: (RenderLayerParent<Player, EntityModel<Player>>) -> RenderLayer<out Player, out EntityModel<out Player>>) {
+        context.getSkin(skin)?.let { (it as LivingEntityRendererAccessor).addLayer(layer(it as LivingEntityRenderer<Player, EntityModel<Player>>)) }
     }
 
     @CynosureInternal
@@ -40,6 +41,6 @@ public class RenderLayerRegistrationEvent(
 
         public fun getSkin(name: String): EntityRenderer<out Player>?
 
-        public fun <T : LivingEntity> getEntity(entity: EntityType<T>): LivingEntityRenderer<in T, *>?
+        public fun <T : LivingEntity> getEntity(entity: EntityType<T>): LivingEntityRenderer<T, EntityModel<T>>?
     }
 }
